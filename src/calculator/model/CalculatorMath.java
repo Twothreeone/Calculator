91209bfb158ae45cfd4cac44418fd57d151efc5d
Ben Charlesworth <bencharlesworth4@gmail.com>
2017-12-18 13:51:51 -0700

    Added function to CalculatorMath to create a list of the terms and to make sure the list is legal.

--- src/calculator/model/CalculatorMath.java
+++ src/calculator/model/CalculatorMath.java
@@ -1,9 +1,68 @@
 package calculator.model;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CalculatorMath
-{
+{	
 	public String calculate(String equation)
 	{
+		List<String> terms = createList(equation);
+		if (!checkList(terms))
+		{
+			return "Syntax Error";
+		}
 		return "42";
+	}
+	
+	private List<String> createList(String equation)
+	{
+		List<String> terms = new ArrayList<String>();
+		int nextSpace;
+		for (int i = 0; i < equation.length(); i++)
+		{
+			nextSpace = equation.indexOf(' ', i);
+			if (nextSpace >= 0)
+			{
+				terms.add(equation.substring(i, nextSpace));
+				i = nextSpace;	
+			}
+			else
+			{
+				terms.add(equation.substring(i));
+				i = equation.length();
+			}
+		}
+		return terms;
+	}
+	
+	private boolean checkList(List<String> terms)
+	{
+		if (terms.size() % 2 == 0)
+		{
+			return false;
+		}
+		for (int i = 0; i < terms.size(); i += 2)
+		{
+			if(!isDouble(terms.get(i)) && !terms.get(i).equals("Ans"))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean isDouble(String term)
+	{
+		try
+		{
+			Double.parseDouble(term);
+		}
+		catch (NumberFormatException error)
+		{
+			return false;
+		}
+		return true;
 	}
 }
